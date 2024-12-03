f0fc22f3d330e989d2432ca0626d5868c4f20df6
iRepo <iraitz.repository@gmail.com>
2024-12-03 13:39:48 +0100

    make item fetch to return 1 item instead of array

--- items-service/src/services/itemService.ts
+++ items-service/src/services/itemService.ts
@@ -1,23 +1,31 @@
-import { Item } from "../types.ts";
-import { client } from "../db/index.ts";
-import { DeleteItemCommand, PutItemCommand, ScanCommand, UpdateItemCommand } from "client-dynamodb";
-import { marshall, unmarshall } from "util-dynamodb";
-import { unmarshallDataFromDB } from "../utils/utils.ts";
-import { validateItemFromRequest, validateNewItemFromRequest } from "../utils/validators.ts";
-import { v1 } from "uuid";
+import { Item } from "../types.ts"
+import { client } from "../db/index.ts"
+import {
+  DeleteItemCommand,
+  PutItemCommand,
+  ScanCommand,
+  UpdateItemCommand,
+} from "client-dynamodb"
+import { marshall, unmarshall } from "util-dynamodb"
+import { unmarshallDataFromDB } from "../utils/utils.ts"
+import {
+  validateItemFromRequest,
+  validateNewItemFromRequest,
+} from "../utils/validators.ts"
+import { v1 } from "uuid"
 
 export const itemsService = {
   getAllItems: async (): Promise<Item[]> => {
     const command = new ScanCommand({
       TableName: "Items",
-    });
+    })
 
-    const data = await client.send(command);
+    const data = await client.send(command)
 
     if (data.Items) {
-      return unmarshallDataFromDB(data) as Item[];
+      return unmarshallDataFromDB(data) as Item[]
     } else {
-      return [];
+      return []
     }
   },
 
@@ -28,13 +36,13 @@ export const itemsService = {
       ExpressionAttributeValues: {
         ":id": { S: id },
       },
-    });
-    const data = await client.send(command);
+    })
+    const data = await client.send(command)
 
     if (data.Items && data.Items?.length > 0) {
-      return unmarshallDataFromDB(data) as Item;
+      return unmarshallDataFromDB(data)[0] as Item
     } else {
-      return undefined;
+      return undefined
     }
   },
   getItemsByCategory: async (category: string): Promise<Item[]> => {
@@ -44,15 +52,20 @@ export const itemsService = {
       ExpressionAttributeValues: {
         ":category": { S: category },
       },
-    });
-    const data = await client.send(command);
+    })
+    const data = await client.send(command)
 
-    return data.Items ? (data.Items.map((item) => unmarshall(item)) as Item[]) : [];
+    return data.Items
+      ? (data.Items.map((item) => unmarshall(item)) as Item[])
+      : []
   },
-  updateItemById: async (id: string, updatedItemFromRequest: Partial<Item>): Promise<Item | undefined> => {
-    const validAttributes = validateItemFromRequest(updatedItemFromRequest);
+  updateItemById: async (
+    id: string,
+    updatedItemFromRequest: Partial<Item>
+  ): Promise<Item | undefined> => {
+    const validAttributes = validateItemFromRequest(updatedItemFromRequest)
     if (validAttributes.UpdateExpression.length === 0) {
-      throw new Error(`Failed to update item with id:${id}`);
+      throw new Error(`Failed to update item with id:${id}`)
     }
 
     const command = new UpdateItemCommand({
@@ -60,29 +73,31 @@ export const itemsService = {
       Key: { id: { S: id } },
       UpdateExpression: `SET ${validAttributes.UpdateExpression.join(", ")}`,
       ExpressionAttributeNames: validAttributes.ExpressionAttributeNames,
-      ExpressionAttributeValues: marshall(validAttributes.ExpressionAttributeValues),
+      ExpressionAttributeValues: marshall(
+        validAttributes.ExpressionAttributeValues
+      ),
       ReturnValues: "ALL_NEW",
-    });
+    })
 
-    const data = await client.send(command);
+    const data = await client.send(command)
 
-    return unmarshall(data.Attributes ?? {}) as Item;
+    return unmarshall(data.Attributes ?? {}) as Item
   },
 
   createItem: async (newItemFromRequest: Item): Promise<Item> => {
     const newItem = marshall({
       ...validateNewItemFromRequest(newItemFromRequest),
       id: `${v1.generate()}`,
-    });
+    })
 
     const command = new PutItemCommand({
       TableName: "Items",
       Item: newItem,
       ReturnValues: "NONE",
-    });
+    })
 
-    await client.send(command);
-    return unmarshall(newItem ?? {}) as Item;
+    await client.send(command)
+    return unmarshall(newItem ?? {}) as Item
   },
 
   deleteItemById: async (id: string) => {
@@ -92,13 +107,14 @@ export const itemsService = {
         id: { S: id },
       },
       ConditionExpression: "attribute_exists(id)",
-    });
+    })
 
     try {
-      await client.send(command);
+      await client.send(command)
       // deno-lint-ignore no-explicit-any
     } catch (error: any) {
-      if (error.name === "ConditionalCheckFailedException") throw new Error(`Could not find item by id ${id}`);
+      if (error.name === "ConditionalCheckFailedException")
+        throw new Error(`Could not find item by id ${id}`)
     }
   },
-};
+}
