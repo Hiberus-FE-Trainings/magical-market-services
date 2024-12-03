5897b5d0a9019e27885190f66cda21a4dcc5d5ba
iRepo <iraitz.repository@gmail.com>
2024-12-03 16:48:24 +0100

    error handling for fetch to other services

--- gateway/src/services/gatewayService.ts
+++ gateway/src/services/gatewayService.ts
@@ -1,10 +1,10 @@
-import { ENV } from "../../config/env.ts";
-import DynamodbClient from "../../db/db.ts";
-import { PutItemCommand } from "client-dynamodb";
-import { v1 } from "uuid";
+import { ENV } from "../../config/env.ts"
+import DynamodbClient from "../../db/db.ts"
+import { PutItemCommand } from "client-dynamodb"
+import { v1 } from "uuid"
 
-import { marshall, unmarshall } from "util-dynamodb";
-import { Trade } from '../types.ts';
+import { marshall, unmarshall } from "util-dynamodb"
+import { Trade } from "../types.ts"
 
 const createTradeEntry = async ({
   buyerId,
@@ -12,27 +12,28 @@ const createTradeEntry = async ({
   itemId,
   amount,
 }: {
-  buyerId: string;
-  sellerId: string;
-  itemId: string;
-  amount: number;
+  buyerId: string
+  sellerId: string
+  itemId: string
+  amount: number
 }): Promise<Trade> => {
+  const newTrade = {
+    id: v1.generate(),
+    amount,
+    buyer_id: buyerId,
+    seller_id: sellerId,
+    item_id: itemId,
+    date: new Date().toISOString().split("T")[0],
+  }
   const command = new PutItemCommand({
     TableName: "Trades",
-    Item: marshall({
-      id: v1.generate(),
-      amount,
-      buyer_id: buyerId,
-      seller_id: sellerId,
-      item_id: itemId,
-      date: new Date().toISOString().split("T")[0],
-    }),
-    ReturnValues: "ALL_NEW",
-  });
+    Item: marshall(newTrade),
+    ReturnValues: "NONE",
+  })
 
-  const data = await DynamodbClient.send(command);
-  return unmarshall(data.Attributes ?? {}) as Trade;
-};
+  await DynamodbClient.send(command)
+  return newTrade as Trade
+}
 
 const triggerBalanceUpdate = async (mageId: string, balance: number) =>
   await fetch(`${ENV.MAGES_URL}/${mageId}`, {
@@ -41,7 +42,7 @@ const triggerBalanceUpdate = async (mageId: string, balance: number) =>
       "Content-Type": "application/json",
     },
     body: JSON.stringify({ balance }),
-  });
+  })
 
 const triggerDeleteItem = async (itemId: string) =>
   await fetch(`${ENV.ITEMS_URL}/${itemId}`, {
@@ -49,7 +50,7 @@ const triggerDeleteItem = async (itemId: string) =>
     headers: {
       "Content-Type": "application/json",
     },
-  });
+  })
 
 const adjustMagesBalance = async ({
   buyerId,
@@ -58,33 +59,48 @@ const adjustMagesBalance = async ({
   buyerBalance,
   sellerBalance,
 }: {
-  buyerId: string;
-  sellerId: string;
-  itemPrice: number;
-  buyerBalance: number;
-  sellerBalance: number;
+  buyerId: string
+  sellerId: string
+  itemPrice: number
+  buyerBalance: number
+  sellerBalance: number
 }) => {
-  const finalSellerBalance = sellerBalance + itemPrice;
-  const finalBuyerBalance = buyerBalance - itemPrice;
+  const finalSellerBalance = sellerBalance + itemPrice
+  const finalBuyerBalance = buyerBalance - itemPrice
 
   if (finalBuyerBalance < 0) {
     throw new Error(
-      `Buyer with ID: ${buyerId} does not have enought balance to proceed with the trade!`,
-    );
+      `Buyer with ID: ${buyerId} does not have enought balance to proceed with the trade!`
+    )
   }
   await Promise.all([
     triggerBalanceUpdate(sellerId, finalSellerBalance),
     triggerBalanceUpdate(buyerId, finalBuyerBalance),
-  ]);
-};
+  ])
+}
 
 export const gatewayService = {
-  itemTrade: async (itemId: string, sellerId: string, buyerId: string) : Promise<Trade> => {
+  itemTrade: async (
+    itemId: string,
+    sellerId: string,
+    buyerId: string
+  ): Promise<Trade> => {
     const [item, seller, buyer] = await Promise.all([
-      (await fetch(`${ENV.ITEMS_URL}/${itemId}`)).json(),
-      (await fetch(`${ENV.MAGES_URL}/${sellerId}`)).json(),
-      (await fetch(`${ENV.MAGES_URL}/${buyerId}`)).json(),
-    ]);
+      fetch(`${ENV.ITEMS_URL}/${itemId}`),
+      fetch(`${ENV.MAGES_URL}/${sellerId}`),
+      fetch(`${ENV.MAGES_URL}/${buyerId}`),
+    ]).then(async ([item, seller, buyer]) => {
+      if (!item.ok) {
+        throw new Error(`Item with ID: ${itemId} was not found!`)
+      }
+      if (!seller.ok) {
+        throw new Error(`Seller with ID: ${sellerId} was not found!`)
+      }
+      if (!buyer.ok) {
+        throw new Error(`Buyer with ID: ${buyerId} was not found!`)
+      }
+      return await Promise.all([item.json(), seller.json(), buyer.json()])
+    })
 
     await adjustMagesBalance({
       buyerId,
@@ -92,14 +108,14 @@ export const gatewayService = {
       itemPrice: item.price,
       sellerBalance: seller.balance,
       buyerBalance: buyer.balance,
-    });
-    await triggerDeleteItem(itemId);
+    })
+    await triggerDeleteItem(itemId)
 
-   return await createTradeEntry({
+    return await createTradeEntry({
       buyerId,
       sellerId,
       amount: item.price,
       itemId: item.id,
-    });
+    })
   },
-};
+}
