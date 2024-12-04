69f78e32cea5d0552fbf7e619b52fd541a421689
ivanIglesiasP <vanluarkwork@gmail.com>
2024-12-04 11:03:18 +0100

    fix: using proper env variables

--- mages-service/config/env.ts
+++ mages-service/config/env.ts
@@ -4,6 +4,7 @@ const env = config();
 
 export const ENV = {
   PORT: Deno.env.get("PORT") || env.PORT,
-  AWS_ACCESS_KEY_ID: Deno.env.get("AWS_ACCESS_KEY_ID") || env.PORT,
-  AWS_SECRET_ACCESS_KEY: Deno.env.get("AWS_SECRET_ACCESS_KEY") || env.PORT,
+  AWS_ACCESS_KEY_ID: Deno.env.get("AWS_ACCESS_KEY_ID") || env.AWS_ACCESS_KEY_ID,
+  AWS_SECRET_ACCESS_KEY: Deno.env.get("AWS_SECRET_ACCESS_KEY") || env.AWS_SECRET_ACCESS_KEY,
+  AWS_TABLE_REGION: Deno.env.get("AWS_TABLE_REGION") || env.AWS_TABLE_REGION,
 };
--- mages-service/src/db/db.ts
+++ mages-service/src/db/db.ts
@@ -2,7 +2,7 @@ import { DynamoDBClient } from "dynamodb";
 import { ENV } from "../../config/env.ts";
 
 const DynamodbClient = new DynamoDBClient({
-  region: "eu-central-1",
+  region: ENV.AWS_TABLE_REGION,
   credentials: {
     accessKeyId: ENV.AWS_ACCESS_KEY_ID || "",
     secretAccessKey: ENV.AWS_SECRET_ACCESS_KEY || "",
